5c86f5405da2260ba6316f5058fe1ca88ebb6288
iwanmitowski <iwan.mitowski@gmail.com>
2021-11-17 13:42:19 +0200

    Creating dashboard leading to other administration pages.

---
+++ src/adnumaZ/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,41 @@
+﻿using adnumaZ.Common.Constants;
+using adnumaZ.Data;
+using adnumaZ.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace adnumaZ.Areas.Administration.Controllers
+{
+    [Authorize(Roles = Constants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public DashboardController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var totalRegisteredUsers = dbContext.UserAccounts.Count();
+            var totalBannedUsers = dbContext.UserAccounts.Count(x => x.IsBanned);
+            var totalTorrents = dbContext.Torrents.Count();
+            var totalUploadedGBs = dbContext.UserAccounts.Sum(x => x.UploadedTorrentGBs);
+            var totalDownloadedGBs = dbContext.UserAccounts.Sum(x => x.DownloadedTorrentGBs);
+            //TODO: Add total comments
+
+            var dashboardViewModel = new DashboardViewModel()
+            {
+                TotalRegisteredUsers = totalRegisteredUsers,
+                TotalBannedUsers = totalBannedUsers,
+                TotalTorrents = totalTorrents,
+                TotalUploadedGBs = totalUploadedGBs,
+                TotalDownloadedGBs = totalDownloadedGBs,
+            };
+
+            return View(dashboardViewModel);
+        }
+    }
+}
---
+++ src/adnumaZ/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adnumaZ.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalRegisteredUsers { get; set; }
+        public int TotalBannedUsers { get; set; }
+        public int TotalTorrents { get; set; }
+        public double TotalUploadedGBs { get; set; }
+        public double TotalDownloadedGBs { get; set; }
+    }
+}
