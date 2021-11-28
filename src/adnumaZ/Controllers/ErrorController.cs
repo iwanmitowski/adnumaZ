27a69c16d354e81d9ee083ef16761dd60f396f92
iwanmitowski <iwan.mitowski@gmail.com>
2021-11-28 22:25:05 +0200

    Custom error pages.

---
+++ src/adnumaZ/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace adnumaZ.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult HttpError(int statusCode)
+        {
+            if ((statusCode < 100 || statusCode > 599) || statusCode == 404)
+            {
+                return View("404ErrorView");
+            }
+
+            return View(statusCode);
+        }
+    }
+}
