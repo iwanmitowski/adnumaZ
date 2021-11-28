3c351c367ba68d11f2ad0150df8868c0083854ec
iwanmitowski <iwan.mitowski@gmail.com>
2021-11-28 18:36:07 +0200

    Editing torrent with modal form.

---
+++ src/adnumaZ/ViewModels/EditTorrentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace adnumaZ.ViewModels
+{
+    public class EditTorrentViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+    }
+}
